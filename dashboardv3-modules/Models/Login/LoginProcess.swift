3c2f2d451d0703f6fadb27dce92418353c38e07a
Mohd Zulhilmi Mohd Zain <m.zulhilmi@msd.net.my>
2017-11-07 14:12:47 +0800

    modify directory again

---
+++ dashboardv3-modules/Models/Login/LoginProcess.swift
@@ -0,0 +1,18 @@
+//
+//  LoginProcess.swift
+//  dashboardv2
+//
+//  Created by Mohd Zulhilmi Mohd Zain on 07/11/2017.
+//  Copyright © 2017 Ingeniworks Sdn Bhd. All rights reserved.
+//
+
+import UIKit
+
+class LoginProcess: NSObject {
+    
+    func processLoginData(parameters: [String : Any]) -> NSDictionary {
+        
+        return ["status":0, "message":"Unknown Error"]
+    }
+
+}
